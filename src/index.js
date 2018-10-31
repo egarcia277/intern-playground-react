a3ece711ffedf512765147680f80e4243137a472
egarcia277 <44629895+egarcia277@users.noreply.github.com>
2018-10-30 22:08:56 -0700

    lookup tables

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import './styles.css';
+import "./styles.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello Esteban</h1>
-      <h2>Start editing to see some react magic happen!</h2>
-      <h3>We are using Github for source control</h3>
+      <h1 className="Title">MEDS4 Lookup Tables</h1>
+      <ul>
+        <li>AMR_Agency</li>
+        <li>AMR_Emp</li>
+        <li>AMR_Zone</li>
+        <li>AMR_LookupDependencies</li>
+      </ul>
     </div>
   );
 }
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
